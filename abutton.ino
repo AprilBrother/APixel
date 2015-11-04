e6ddf69e0fc91d9cb2e25a82d26838e614fd0339
volca <volca@tom.com>
2015-11-04 19:59:39 +0800

    abutton

--- abutton.ino
+++ abutton.ino
@@ -2,12 +2,29 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 
-void setup() {
-  // put your setup code here, to run once:
+const char *ssid = "AButton";
 
+void setup() {
+  Serial.begin(115200);
+
+  initWiFi();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void initWiFi() {
+  Serial.println();
+  Serial.println();
+  Serial.println("Startup");
+
+
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid);
+  WiFi.begin(ssid); // not sure if need but works
+  Serial.print("Access point started with name ");
+  Serial.println(ssid);
+  //launchWeb(2);
 }
