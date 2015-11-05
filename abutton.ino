fc46445628ee4bf808ea27780f9632c81e0bf43c
volca <volca@tom.com>
2015-11-05 08:46:36 +0800

    conf

--- abutton.ino
+++ abutton.ino
@@ -1,8 +1,12 @@
+#include <Arduino.h>
+
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 
 const char *ssid = "AButton";
+
+ESP8266WebServer server(80);
 
 void setup() {
   Serial.begin(115200);
@@ -26,5 +30,6 @@ void initWiFi() {
   WiFi.begin(ssid); // not sure if need but works
   Serial.print("Access point started with name ");
   Serial.println(ssid);
-  //launchWeb(2);
+
+  server.on("/", handleConfig);
 }
